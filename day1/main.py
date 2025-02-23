9662e86ecd6f7bb9bc602d0af88f86f81c9f18e0
Muhammad Habib <mhabib34official@gmail.com>
2025-02-23 22:41:55 +0700

    Add files via upload

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+# exercise 1
+# print('Hello World')
+# exercise 2
+integer_var = 10
+print(f'This integer is {integer_var}')
+float_var = 3.14
+print(f'This float is {float_var}')
+string_var = 'muhammad habib' 
+print(f'This string is {string_var}')
+list_var = [10,'Aku',True]
+list_var.append([12,'Dia',False])
+print(f'This list is {list_var}, {list_var[3][1]}')
+list_var.remove('Aku')
+print(f'This list is {list_var}, {list_var[2][1]}')
+tuple_var = (10,9,8,7)
+print(f'This tuple is {tuple_var}')
+boolean_var = True
+print(f'This boolean is {boolean_var}')
+dict_var = {"nama": 'Muhammad Habib','umur':19}
+print(f'Nama saya {dict_var["nama"]} umur saya {dict_var["umur"]}')
